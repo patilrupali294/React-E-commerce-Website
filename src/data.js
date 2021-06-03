24985d237577bde4c431657e57b01596159f4310
rupali <pailrupali294@gmail.com>
2021-06-03 14:21:39 +0200

    first commit

--- src/data.js
+++ src/data.js
@@ -5,7 +5,7 @@ export const storeProducts = [
     title: "Aloe vera",
     img: "img/product-1.png",
     price: 10,
-    company: "GOOGLE",
+    company: "Rupdarshan Contractor",
     info:
       "Aloe vera belongs to the Asphodelaceae family. Aloe vera cultivated for agricultural and medicinal uses. Aloe vera also used for decorative purposes and grows successfully indoors and outdoor. The leaves are thick and fleshy, green to grey-green. It is found in many consumer products including beverages, skin lotion, cosmetics, or ointments for minor burns and sunburns.Note: Any variety of these plants will be delivered, a few shown in the photos.",
     inCart: false,
@@ -17,7 +17,7 @@ export const storeProducts = [
     title: "Scented rose",
     img: "img/product-2.png",
     price: 16,
-    company: "SAMSUNG",
+    company: "Rupdarshan Contractor",
     info:
       "Scented roses are known for its distinct fragrance. The rose is a perennial flowering plant. The flower represents symbol and meaning",
     inCart: false,
@@ -26,72 +26,72 @@ export const storeProducts = [
   },
   {
     id: 3,
-    title: "HTC 10 - Black",
+    title: "Best 4 plants to Kill Indoor Pollution",
     img: "img/product-3.png",
     price: 8,
-    company: "htc",
+    company: "Rupdarshan Contractor",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "This pack contains 4 air purifier plants along with 4 color pots. Surround your home with these best pollution killer plants for clean and healthy environment.",
     inCart: false,
     count: 0,
     total: 0
   },
   {
     id: 4,
-    title: "HTC 10 - White",
+    title: "Money plant, Scindapsus Green ",
     img: "img/product-4.png",
     price: 18,
-    company: "htc",
+    company: "Rupdarshan Contractor",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Money plant is very easy to grow plant which needs less maintenance. You can hang the potted Money plant with the help of the given hanger to decorate your balcony, terrace, etc. Hanging baskets can bring a sense of serenity and beauty to your environment. Money plant comes in NASA air purifier list and according to Feng Shui regarded as the lucky plant. It is believed to bring prosperity and good luck. Money plants are known as Devils wine as these plants are very hard to kill.",
     inCart: false,
     count: 0,
     total: 0
   },
   {
     id: 5,
-    title: "HTC Desire 626s",
+    title: "Teardrop Peperomia, Peperomia orba",
     img: "img/product-5.png",
     price: 24,
-    company: "htc",
+    company: "Rupdarshan Contractor",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Peperomia is a delightful, easy-to-grow houseplant that's been around for years and it's still popular. Part of the reason for peperomia's popularity is that it's such an easy indoor plant. Plus, it's one of the better small houseplants, so you don't need a lot of room to grow it.",
     inCart: false,
     count: 0,
     total: 0
   },
   {
     id: 6,
-    title: "Vintage Iphone",
+    title: "3 Layer Lucky Bamboo Plant in a Bowl with Pebbles ",
     img: "img/product-6.png",
     price: 17,
-    company: "apple",
+    company: "Rupdarshan Contractor",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Every gift is a wish of happiness for your loved ones. Wish all happiness in their life and gift them 3 layer lucky bamboo plant along with pebbles.",
     inCart: false,
     count: 0,
     total: 0
   },
   {
     id: 7,
-    title: "Iphone 7",
+    title: "Hibiscus, Gudhal Flower",
     img: "img/product-7.png",
     price: 30,
-    company: "apple",
+    company: "Rupdarshan Contractor",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Hibiscus are delightful shrubs with dark green leaves. Giving a hibiscus meant that the giver was acknowledging the receivers delicate beauty.",
     inCart: false,
     count: 0,
     total: 0
   },
   {
     id: 8,
-    title: "Smashed Iphone",
+    title: "Flower of West Bengal, Harshringar",
     img: "img/product-8.png",
     price: 2,
-    company: "apple",
+    company: "Rupdarshan Contractor",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Night Flowering Jasmine, Harshringar or Parijat is West Bengal state flower. It produces adorable and sweetly fragrant flowers which open at night. ",
     inCart: false,
     count: 0,
     total: 0
